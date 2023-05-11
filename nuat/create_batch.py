8499e628e5f7c45b18be90e239d5749b80e57b9a
Jeremy Magland <jeremy.magland@gmail.com>
2023-05-11 10:22:08 -0400

    filtered only

--- nuat/create_batch.py
+++ nuat/create_batch.py
@@ -14,6 +14,7 @@ from .helpers.extract_snippets import extract_snippets_in_channel_neighborhood
 
 def create_batch(*,
     recording: si.BaseRecording,
+    recording_filtered_only: Union[si.BaseRecording, None]=None,
     sorting: si.BaseSorting,
     batch_id: str,
     output_folder: str,
@@ -64,7 +65,6 @@ def create_batch(*,
     
     for unit_id in unit_ids:
         print(f'Processing unit {unit_id}...')
-        unit_index = unit_ids.index(unit_id)
         unit_folder = f'{output_folder}/units/{unit_id}'
         os.makedirs(unit_folder)
         channel_neighborhood = [x for x in channel_neighborhoods if x['unit_id'] == unit_id][0]
@@ -72,14 +72,23 @@ def create_batch(*,
         spike_times_sec = spike_times / recording.get_sampling_frequency()
         autocorrelogram = compute_correlogram_data(sorting=sorting, unit_id1=unit_id, window_size_msec=100, bin_size_msec=1)
         snippets_in_neighborhood = extract_snippets_in_channel_neighborhood(traces=recording.get_traces(), times=spike_times, neighborhood=channel_neighborhood["channel_indices"], T1=30, T2=30)
+        if recording_filtered_only is not None:
+            snippets_in_neighborhood_filtered_only = extract_snippets_in_channel_neighborhood(traces=recording_filtered_only.get_traces(), times=spike_times, neighborhood=channel_neighborhood["channel_indices"], T1=30, T2=30)
+            
         channel_neighborhood_indices = channel_neighborhood['channel_indices']
         channel_locations_in_neighborhood = np.array(recording.get_channel_locations())[channel_neighborhood_indices]
 
         average_waveform_in_neighborhood = np.median(snippets_in_neighborhood, axis=0)
+        if recording_filtered_only is not None:
+            average_waveform_in_neighborhood_filtered_only = np.median(snippets_in_neighborhood_filtered_only, axis=0)
 
         # spike amplitudes
-        peak_channel_index_in_neighborhood = channel_neighborhood["channel_ids"].index(channel_neighborhood["peak_channel_id"])
+        peak_channel_index_in_neighborhood = np.argmin(np.min(average_waveform_in_neighborhood, axis=0))
         spike_amplitudes = np.min(snippets_in_neighborhood[:, :, peak_channel_index_in_neighborhood], axis=1)
+
+        if recording_filtered_only is not None:
+            peak_channel_index_in_neighborhood_filtered_only = np.argmin(np.min(average_waveform_in_neighborhood_filtered_only, axis=0))
+            spike_amplitudes_filtered_only = np.min(snippets_in_neighborhood_filtered_only[:, :, peak_channel_index_in_neighborhood_filtered_only], axis=1)
 
         # write unit_info.json
         unit_info = {
@@ -98,10 +107,16 @@ def create_batch(*,
         data_zarr_root_group = zarr.open(data_zarr_fname, mode="w")
         data_zarr_root_group.create_dataset("spike_times", data=spike_times_sec.astype(np.float32), chunks=(100000,))
         data_zarr_root_group.create_dataset("average_waveform_in_neighborhood", data=average_waveform_in_neighborhood.astype(np.float32), chunks=(1000, 1000))
+        if recording_filtered_only is not None:
+            data_zarr_root_group.create_dataset("average_waveform_in_neighborhood_filtered_only", data=average_waveform_in_neighborhood_filtered_only.astype(np.float32), chunks=(1000, 1000))
         data_zarr_root_group.create_dataset("autocorrelogram_bin_edges_sec", data=autocorrelogram['bin_edges_sec'], chunks=(1000,))
         data_zarr_root_group.create_dataset("autocorrelogram_bin_counts", data=autocorrelogram['bin_counts'], chunks=(1000,))
         data_zarr_root_group.create_dataset("snippets_in_neighborhood", data=snippets_in_neighborhood.astype(np.float32), chunks=(1000, 100, 50))
+        if recording_filtered_only is not None:
+            data_zarr_root_group.create_dataset("snippets_in_neighborhood_filtered_only", data=snippets_in_neighborhood_filtered_only.astype(np.float32), chunks=(1000, 100, 50))
         data_zarr_root_group.create_dataset("spike_amplitudes", data=spike_amplitudes.astype(np.float32), chunks=(10000,))
+        if recording_filtered_only is not None:
+            data_zarr_root_group.create_dataset("spike_amplitudes_filtered_only", data=spike_amplitudes_filtered_only.astype(np.float32), chunks=(10000,))
 
     # Computing unit template correlations
     print('Computing unit template correlations...')
@@ -146,32 +161,57 @@ def create_batch(*,
 
         snippets_1_in_neighborhood = extract_snippets_in_channel_neighborhood(traces=recording.get_traces(), times=spike_times_1, neighborhood=channel_neighborhood_channel_indices, T1=30, T2=30)
         snippets_2_in_neighborhood = extract_snippets_in_channel_neighborhood(traces=recording.get_traces(), times=spike_times_2, neighborhood=channel_neighborhood_channel_indices, T1=30, T2=30)
+        if recording_filtered_only is not None:
+            snippets_1_in_neighborhood_filtered_only = extract_snippets_in_channel_neighborhood(traces=recording_filtered_only.get_traces(), times=spike_times_1, neighborhood=channel_neighborhood_channel_indices, T1=30, T2=30)
+            snippets_2_in_neighborhood_filtered_only = extract_snippets_in_channel_neighborhood(traces=recording_filtered_only.get_traces(), times=spike_times_2, neighborhood=channel_neighborhood_channel_indices, T1=30, T2=30)
 
         average_waveform_1_in_neighborhood = np.median(snippets_1_in_neighborhood, axis=0)
         average_waveform_2_in_neighborhood = np.median(snippets_2_in_neighborhood, axis=0)
+        if recording_filtered_only is not None:
+            average_waveform_1_in_neighborhood_filtered_only = np.median(snippets_1_in_neighborhood_filtered_only, axis=0)
+            average_waveform_2_in_neighborhood_filtered_only = np.median(snippets_2_in_neighborhood_filtered_only, axis=0)
 
         V1s = snippets_1_in_neighborhood.reshape((snippets_1_in_neighborhood.shape[0], snippets_1_in_neighborhood.shape[1] * snippets_1_in_neighborhood.shape[2]))
         V2s = snippets_2_in_neighborhood.reshape((snippets_2_in_neighborhood.shape[0], snippets_2_in_neighborhood.shape[1] * snippets_2_in_neighborhood.shape[2]))
+        if recording_filtered_only is not None:
+            V1s_filtered_only = snippets_1_in_neighborhood_filtered_only.reshape((snippets_1_in_neighborhood_filtered_only.shape[0], snippets_1_in_neighborhood_filtered_only.shape[1] * snippets_1_in_neighborhood_filtered_only.shape[2]))
+            V2s_filtered_only = snippets_2_in_neighborhood_filtered_only.reshape((snippets_2_in_neighborhood_filtered_only.shape[0], snippets_2_in_neighborhood_filtered_only.shape[1] * snippets_2_in_neighborhood_filtered_only.shape[2]))
 
         V1_mean = np.mean(V1s, axis=0)
         V2_mean = np.mean(V2s, axis=0)
+        if recording_filtered_only is not None:
+            V1_mean_filtered_only = np.mean(V1s_filtered_only, axis=0)
+            V2_mean_filtered_only = np.mean(V2s_filtered_only, axis=0)
 
         # direction of discrimination
         direction_of_discrimination = (V2_mean - V1_mean) / np.linalg.norm(V2_mean - V1_mean)
+        if recording_filtered_only is not None:
+            direction_of_discrimination_filtered_only = (V2_mean_filtered_only - V1_mean_filtered_only) / np.linalg.norm(V2_mean_filtered_only - V1_mean_filtered_only)
 
         # inner products with direction of discrimination
         discrimination_features_1 = np.dot(V1s, direction_of_discrimination)
         discrimination_features_2 = np.dot(V2s, direction_of_discrimination)
+        if recording_filtered_only is not None:
+            discrimination_features_1_filtered_only = np.dot(V1s_filtered_only, direction_of_discrimination_filtered_only)
+            discrimination_features_2_filtered_only = np.dot(V2s_filtered_only, direction_of_discrimination_filtered_only)
 
         # subtract off the direction of discrimination
         V1s_orth = V1s - np.outer(discrimination_features_1, direction_of_discrimination)
         V2s_orth = V2s - np.outer(discrimination_features_2, direction_of_discrimination)
+        if recording_filtered_only is not None:
+            V1s_orth_filtered_only = V1s_filtered_only - np.outer(discrimination_features_1_filtered_only, direction_of_discrimination_filtered_only)
+            V2s_orth_filtered_only = V2s_filtered_only - np.outer(discrimination_features_2_filtered_only, direction_of_discrimination_filtered_only)
 
         # find the first principal component of the union of V1s_orth and V2s_orth
         V1s_orth_V2s_orth = np.concatenate((V1s_orth, V2s_orth), axis=0)
         U, S, Vh = np.linalg.svd(V1s_orth_V2s_orth, full_matrices=False)
         pca_features_1 = np.dot(V1s_orth, Vh[0, :])
         pca_features_2 = np.dot(V2s_orth, Vh[0, :])
+        if recording_filtered_only is not None:
+            V1s_orth_V2s_orth_filtered_only = np.concatenate((V1s_orth_filtered_only, V2s_orth_filtered_only), axis=0)
+            U_filtered_only, S_filtered_only, Vh_filtered_only = np.linalg.svd(V1s_orth_V2s_orth_filtered_only, full_matrices=False)
+            pca_features_1_filtered_only = np.dot(V1s_orth_filtered_only, Vh_filtered_only[0, :])
+            pca_features_2_filtered_only = np.dot(V2s_orth_filtered_only, Vh_filtered_only[0, :])
 
         # write unit_pair_info.json
         unit_pair_info = {
@@ -194,12 +234,21 @@ def create_batch(*,
         data_zarr_root_group.create_dataset("spike_times_2", data=spike_times_sec_2.astype(np.float32), chunks=(100000,))
         data_zarr_root_group.create_dataset("average_waveform_1_in_neighborhood", data=average_waveform_1_in_neighborhood.astype(np.float32), chunks=(1000, 1000))
         data_zarr_root_group.create_dataset("average_waveform_2_in_neighborhood", data=average_waveform_2_in_neighborhood.astype(np.float32), chunks=(1000, 1000))
+        if recording_filtered_only is not None:
+            data_zarr_root_group.create_dataset("average_waveform_1_in_neighborhood_filtered_only", data=average_waveform_1_in_neighborhood_filtered_only.astype(np.float32), chunks=(1000, 1000))
+            data_zarr_root_group.create_dataset("average_waveform_2_in_neighborhood_filtered_only", data=average_waveform_2_in_neighborhood_filtered_only.astype(np.float32), chunks=(1000, 1000))
         data_zarr_root_group.create_dataset("cross_correlogram_bin_edges_sec", data=cross_correlogram['bin_edges_sec'], chunks=(1000,))
         data_zarr_root_group.create_dataset("cross_correlogram_bin_counts", data=cross_correlogram['bin_counts'], chunks=(1000,))
         data_zarr_root_group.create_dataset("discrimination_features_1", data=discrimination_features_1.astype(np.float32), chunks=(10000,))
         data_zarr_root_group.create_dataset("discrimination_features_2", data=discrimination_features_2.astype(np.float32), chunks=(10000,))
+        if recording_filtered_only is not None:
+            data_zarr_root_group.create_dataset("discrimination_features_1_filtered_only", data=discrimination_features_1_filtered_only.astype(np.float32), chunks=(10000,))
+            data_zarr_root_group.create_dataset("discrimination_features_2_filtered_only", data=discrimination_features_2_filtered_only.astype(np.float32), chunks=(10000,))
         data_zarr_root_group.create_dataset("pca_features_1", data=pca_features_1.astype(np.float32), chunks=(10000,))
         data_zarr_root_group.create_dataset("pca_features_2", data=pca_features_2.astype(np.float32), chunks=(10000,))
+        if recording_filtered_only is not None:
+            data_zarr_root_group.create_dataset("pca_features_1_filtered_only", data=pca_features_1_filtered_only.astype(np.float32), chunks=(10000,))
+            data_zarr_root_group.create_dataset("pca_features_2_filtered_only", data=pca_features_2_filtered_only.astype(np.float32), chunks=(10000,))
 
     # create batch_info.json
     batch_info = {
@@ -209,7 +258,8 @@ def create_batch(*,
         'num_frames': int(recording.get_num_frames()),
         'unit_ids': unit_ids,
         'unit_pair_ids': unit_pair_ids,
-        'channel_locations': serialize_channel_locations(recording.get_channel_locations())
+        'channel_locations': serialize_channel_locations(recording.get_channel_locations()),
+        'filtered_only_included': recording_filtered_only is not None
     }
     print(f'Num. channels: {len(channel_ids)}')
     print(f'Num. units: {len(unit_ids)}')
